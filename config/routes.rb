914161b0a6940a0ab0bc2acffa73d4d103f9e117
Chad Lengefeld <chadlengefeld@gmail.com>
2021-05-16 06:22:38 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Add your routes here
   root 'welcome#home'
 
-  # get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#create'
 end
